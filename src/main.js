71934c31496f8e06551ab2856612dd506ddf29f0
liumapp <liumapp.com@gmail.com>
2018-04-16 14:49:47 +0800

    add vuex & style.css

--- src/main.js
+++ src/main.js
@@ -1,8 +1,18 @@
+/**
+ * @file main.js
+ * @author liumapp
+ * @email liumapp.com@gmail.com
+ * @homepage http://www.liumapp.com
+ * @date 4/16/18
+ */
+
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import Vuex from 'vuex'
 
 Vue.config.productionTip = false
 
